090a759e2b7e1853f612448d9e92a14dfa380a41
limeooo <2978582971@qq.com>
2022-03-22 23:13:34 +0800

    feat(css): 公共样式引入

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
+import './assets/css/index.less'
 import 'virtual:svg-icons-register'
 
 const app = createApp(App)
