16baf5e5a3f025e655018acb701726acc853a5f6
limeooo <2978582971@qq.com>
2022-03-23 22:14:22 +0800

    feat(latest): latest页面 歌曲列表组件封装

---
+++ src/components/song-list/SongList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="song-list">
+    <template v-for="item in songList" :key="item.id">
+      <SongListItems :song-item="item" />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import SongListItems from './SongListItems.vue'
+import { withDefaults, defineProps } from 'vue'
+import { ISong } from '@/service/latest/types'
+
+withDefaults(
+  defineProps<{
+    songList: ISong[]
+  }>(),
+  {}
+)
+</script>
---
+++ src/components/song-list/SongListItems.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="song-list-item">
+    <div class="item-order">{{ padZero(songItem.order) }}</div>
+    <div class="item-image">
+      <img v-lazy="thumbnail(songItem.picUrl, 60)" />
+    </div>
+    <div class="item-text text-ellipsis">{{ songItem.name }}</div>
+    <div class="item-text text-ellipsis">{{ songItem.artists }}</div>
+    <div class="item-text text-ellipsis">{{ songItem.album }}</div>
+    <div class="item-time">{{ formatDuration(songItem.duration) }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { withDefaults, defineProps } from 'vue'
+import { ISong } from '@/service/latest/types'
+import { padZero, formatDuration, thumbnail } from '@/utils'
+
+withDefaults(
+  defineProps<{
+    songItem: ISong
+  }>(),
+  {}
+)
+</script>
+
+<style lang="less" scoped>
+.song-list-item {
+  height: 76px;
+  padding: 8px 0;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  &:hover {
+    background-color: var(--color-avtive-background-light);
+  }
+  .item-order,
+  .item-time {
+    width: 60px;
+    font-size: 12px;
+    text-align: center;
+  }
+  .item-order {
+    color: var(--color-text-light);
+  }
+  .item-image {
+    width: 60px;
+    height: 60px;
+    margin-right: 8px;
+    img {
+      width: 60px;
+      height: 60px;
+    }
+  }
+  .item-text {
+    flex: 1;
+    margin-left: 20px;
+    font-size: 12px;
+    color: var(--color-text);
+  }
+}
+</style>
