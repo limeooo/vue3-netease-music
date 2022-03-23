c43463bca01f3ff5d576e8c2e740cd069d56b0da
limeooo <2978582971@qq.com>
2022-03-23 16:25:16 +0800

    feat(menu): menu 菜单样式搭建

---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="menu">
+    <MenuUser />
+    <MenuList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import MenuUser from './components/MenuUser.vue'
+import MenuList from './components/MenuList.vue'
+</script>
+
+<style lang="less" scoped>
+.menu {
+  position: fixed;
+  top: 50px;
+  left: 0;
+  bottom: 60px;
+  width: 250px;
+  background-color: var(--color-menu-background);
+}
+</style>
---
+++ src/components/menu/components/MenuUser.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="menu-user">
+    <div class="user-avatar">
+      <el-avatar :icon="StarFilled" />
+    </div>
+    <div class="user-name">
+      <span>{{ userName }}</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { StarFilled } from '@element-plus/icons-vue'
+
+const userName = ref('登录')
+</script>
+
+<style lang="less" scoped>
+.menu-user {
+  height: 72px;
+  padding: 16px;
+  display: flex;
+  align-items: center;
+  .user-avatar {
+    .el-avatar {
+      background-color: var(--color-main);
+    }
+  }
+  .user-name {
+    font-size: 15px;
+    padding-left: 10px;
+  }
+}
+</style>
