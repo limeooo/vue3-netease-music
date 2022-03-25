5041036f14a625c56bcf92703ee912afef342201
limeooo <2978582971@qq.com>
2022-03-25 09:00:32 +0800

    feat(latest): 歌曲列表组件更名

--- src/components/song-list/SongList.vue
+++ src/components/song-list/SongList.vue
@@ -2,13 +2,13 @@
   <!-- 歌曲列表展示封装组件 -->
   <div class="song-list">
     <template v-for="item in songList" :key="item.id">
-      <SongListItems :song-item="item" />
+      <SongListItem :song-item="item" />
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
-import SongListItems from './SongListItems.vue'
+import SongListItem from './SongListItem.vue'
 
 import { withDefaults } from 'vue'
 import type { ISong } from '@/service/latest/types'
