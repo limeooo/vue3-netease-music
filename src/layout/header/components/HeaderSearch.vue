c8d10c33322974035abee7d936034757d7c801af
limeooo <2978582971@qq.com>
2022-03-26 23:26:37 +0800

    feat(search): 点击 搜索框 弹出页

--- src/layout/header/components/HeaderSearch.vue
+++ src/layout/header/components/HeaderSearch.vue
@@ -1,20 +1,33 @@
 <template>
-  <div class="header-search">
+  <div ref="headerSearchRef" class="header-search">
     <el-input
       v-model="inputValue"
       size="default"
       placeholder="搜索"
       :prefix-icon="Search"
       clearable
+      @focus="isFocus = true"
     />
+    <HeaderSearchTags v-show="isFocus" />
   </div>
 </template>
 
 <script setup lang="ts">
+import HeaderSearchTags from './HeaderSearchTags.vue'
+
 import { ref } from 'vue'
+import { onClickOutside } from '@vueuse/core'
 import { Search } from '@element-plus/icons-vue'
 
 const inputValue = ref('')
+const isFocus = ref(false)
+const headerSearchRef = ref<HTMLDivElement>()
+/**
+ * "onClickOutside" 监听元素外的点击事件
+ */
+onClickOutside(headerSearchRef, () => {
+  isFocus.value = false
+})
 </script>
 
 <style lang="less" scoped>
---
+++ src/layout/header/components/HeaderSearchTags.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header-search-tags"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.header-search-tags {
+  position: fixed;
+  top: 50px;
+  right: 0;
+  bottom: 63px;
+  width: 350px;
+  z-index: var(--z-index-search);
+  background: var(--color-background);
+  box-shadow: 0 5px 5px 0 rgb(0, 0, 0, 0.2);
+}
+</style>
