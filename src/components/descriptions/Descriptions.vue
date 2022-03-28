0f18ca0f4e79f307f9d31cebcef0e0ab8e526dda
limeooo <2978582971@qq.com>
2022-03-28 20:37:28 +0800

    feat(descriptions): 描述列表组件封装

---
+++ src/components/descriptions/Descriptions.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="description">
+    <el-descriptions :title="title" :column="column" border>
+      <template v-for="(item, index) in tableData" :key="index">
+        <el-descriptions-item>
+          <template #label> {{ item.name }} </template>
+          <el-tag size="small">{{ item.value }}</el-tag>
+        </el-descriptions-item>
+      </template>
+    </el-descriptions>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { withDefaults } from 'vue'
+import { IDescription } from '@/global/types'
+
+withDefaults(
+  defineProps<{
+    title?: string
+    column?: number
+    tableData: IDescription[]
+  }>(),
+  {
+    title: '',
+    column: 1
+  }
+)
+</script>
+
+<style lang="less" scoped>
+.description {
+  margin-bottom: 20px;
+}
+</style>
--- src/global/types.ts
+++ src/global/types.ts
@@ -11,3 +11,11 @@ export interface IMultiPageConfig {
   limit?: string | number
   offset?: string | number
 }
+
+/**
+ * description描述类型
+ */
+export interface IDescription {
+  name: string
+  value: string
+}
