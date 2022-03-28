7ce1f71f06f23d05ab76167d1a54c08ff8fdd30f
limeooo <2978582971@qq.com>
2022-03-28 23:17:53 +0800

    feat(header): 顶部 跳转按钮逻辑

--- src/layout/header/components/HeaderNavgitor.vue
+++ src/layout/header/components/HeaderNavgitor.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="header-navgitor">
-    <SvgIcon name="arrow-back" />
-    <SvgIcon name="arrow-forward" />
+    <SvgIcon name="arrow-back" @click="hendleRouterBack()" />
+    <SvgIcon name="arrow-forward" @click="hendleRouterForward()" />
   </div>
 </template>
 
 <script setup lang="ts">
 import SvgIcon from '@/components/base/SvgIcon.vue'
+
+import { useRouter } from 'vue-router'
+import { usePlayerStore } from '@/store'
+
+const router = useRouter()
+const playerStore = usePlayerStore()
+const hendleRouterBack = () => {
+  playerStore.isOpenLyric ? (playerStore.isOpenLyric = false) : router.back()
+}
+const hendleRouterForward = () => {
+  playerStore.isOpenLyric ? (playerStore.isOpenLyric = false) : router.forward()
+}
 </script>
 
 <style lang="less" scoped>
