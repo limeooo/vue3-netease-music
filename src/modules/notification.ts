1e352965f83cdb1089fd75369d97ca0c054115be
limeooo <2978582971@qq.com>
2022-03-29 21:47:21 +0800

    feat(tip): update

--- src/modules/notification.ts
+++ src/modules/notification.ts
@@ -8,7 +8,7 @@ ElNotification({
   type: 'warning',
   title: '提示',
   message:
-    '由于本站音乐为直接获取网易云Api接口数据、导致有小部分音乐因不可控原因无法正常播放、敬请谅解～',
+    '由于本站音乐为直接获取网易云API接口数据、导致VIP音乐无法正常播放和下载、敬请谅解～',
   duration: 0,
   offset: 50
 })
