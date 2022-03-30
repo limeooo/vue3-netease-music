b246bf0f174596cc1fa4261a20375e95ae8c3bf2
limeooo <2978582971@qq.com>
2022-03-30 21:41:18 +0800

    feat(svg): svg-icon组件封装

--- src/components/base/SvgIcon.vue
+++ src/components/base/SvgIcon.vue
@@ -1,12 +1,12 @@
 <template>
   <!-- 公用Svg组件，需引入vite-plugin-svg-icons包 ，官网：https://github.com/vbenjs/vite-plugin-svg-icons -->
   <svg class="icon" :class="className" aria-hidden="true" :style="getStyle">
-    <use :xlink:href="symbolId" />
+    <use :xlink:href="symbolId" :fill="color" />
   </svg>
 </template>
 
 <script setup lang="ts">
-import { withDefaults, computed } from 'vue'
+import { withDefaults, computed, ref } from 'vue'
 import type { CSSProperties } from 'vue'
 
 const props = withDefaults(
@@ -14,22 +14,39 @@ const props = withDefaults(
     name: string
     prefix?: string
     size?: number | string
+    color?: string
   }>(),
   {
     prefix: 'icon',
-    size: 16
+    size: 16,
+    color: '#333'
   }
 )
 
+// 动态绑定class
 const className = computed(() => `${props.prefix}-${props.name}`)
+// 动态绑定id
 const symbolId = computed(() => `#${props.prefix}-${props.name}`)
+
+/**
+ * 根据屏幕大小获取svg-icon尺寸大小
+ */
+const _size = ref(16)
+const getSize = () => {
+  const size = `${props.size}`
+  let s = Number(size.replace('px', ''))
+  _size.value = (document.documentElement.clientWidth / 1440) * s
+}
+getSize()
+
+/**
+ * 监听屏幕变化改变大小
+ */
+window.addEventListener('resize', getSize)
 const getStyle = computed((): CSSProperties => {
-  const { size } = props
-  let s = `${size}`
-  s = `${s.replace('px', '')}px`
   return {
-    width: s,
-    height: s
+    width: _size.value,
+    height: _size.value
   }
 })
 </script>
