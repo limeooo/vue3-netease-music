7654ae2b5e6d9c6b5175cef9a16e384c0fc4c673
limeooo <2978582971@qq.com>
2022-03-31 20:54:45 +0800

    feat(fullscreen): 全屏

--- src/layout/header/components/HeaderButton.vue
+++ src/layout/header/components/HeaderButton.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="header-button">
     <el-tooltip effect="light" content="你" placement="bottom">
-      <span class="mac-icon mac-icon--red"></span>
+      <span class="mac-icon mac-icon--red" @click="$router.push('/discovery')">
+        <SvgIcon name="header-home" size="13" />
+      </span>
     </el-tooltip>
     <el-tooltip effect="light" content="好" placement="bottom">
-      <span class="mac-icon mac-icon--yellow"></span>
+      <span class="mac-icon mac-icon--yellow" @click="exit">
+        <SvgIcon name="header-fullscreen-cancel" size="13" />
+      </span>
     </el-tooltip>
     <el-tooltip effect="light" content="啊" placement="bottom">
-      <span class="mac-icon mac-icon--green"></span>
+      <span class="mac-icon mac-icon--green" @click="toggle">
+        <SvgIcon name="header-fullscreen" size="13" />
+      </span>
     </el-tooltip>
   </div>
 </template>
+
+<script setup lang="ts">
+import SvgIcon from '@/components/base/SvgIcon.vue'
+import { useFullscreen } from '@vueuse/core'
+
+const { toggle, exit } = useFullscreen()
+</script>
 
 <style lang="less" scoped>
 .header-button {
@@ -18,6 +31,7 @@
   width: 79px;
   padding: 0 10px;
   .mac-icon {
+    display: inline-block;
     .border-radius(13px);
     cursor: pointer;
     &.mac-icon--red {
@@ -28,6 +42,16 @@
     }
     &.mac-icon--green {
       background-color: @color-mac-button-green;
+    }
+    .icon {
+      .display-flex(center,center);
+      opacity: 0;
+      transition: all 0.3s;
+    }
+    &:hover {
+      .icon {
+        opacity: 1;
+      }
     }
   }
 }
