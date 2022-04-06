f58ff54ac8fae71bfa6731716f148f7cd319e9d1
limeooo <2978582971@qq.com>
2022-04-06 20:13:23 +0800

    feat(player): 播放列表样式修改

--- src/layout/player/components/PlayerList.vue
+++ src/layout/player/components/PlayerList.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="player-list" :class="{ 'is-hidden': !isOpenPlayerList }">
-    <!-- <div class="header">
-        <div class="close" @click="handleClosePlayerList()">
-          <SvgIcon name="close" size="16" />
-        </div>
-        <div class="delete-all" @click="handleDeletePlayerSong(-1)">
-          <SvgIcon name="rubbish" />
-          <span>清除</span>
-        </div>
-      </div> -->
+    <div class="header">
+      <div class="list-info">
+        {{ `总共${currentPlayerSongList.length}首` }}
+      </div>
+      <div class="delete-all" @click="handleDeletePlayerSong(-1)">
+        <SvgIcon name="rubbish" />
+        <span>清空</span>
+      </div>
+    </div>
     <div class="list">
       <template v-if="currentPlayerSongList.length === 0">
         <el-empty description="列表为空～" />
@@ -20,7 +20,7 @@
             :class="{ isActive: currentPlayerSong.id === item.id }"
             @click="handlePlayListItemClick(index)"
           >
-            <span class="text order">{{ padZero(index + 1) }}</span>
+            <!-- <span class="text order">{{ padZero(index + 1) }}</span> -->
             <span class="text name">{{ item.name }}</span>
             <span class="text artists">{{ item.artists }}</span>
             <span class="text time">{{ formatDuration(item.duration) }}</span>
@@ -41,7 +41,7 @@
 </template>
 
 <script setup lang="ts">
-import { formatDuration, padZero } from '@/utils'
+import { formatDuration } from '@/utils'
 import { storeToRefs } from 'pinia'
 import { usePlayerStore } from '@/store'
 
@@ -56,9 +56,9 @@ const handlePlayListItemClick = (index: number) => {
 }
 
 // // 监听删除播放列表歌曲
-// const handleDeletePlayerSong = (index: number) => {
-//   playerStore.deletePlayerSong(index)
-// }
+const handleDeletePlayerSong = (index: number) => {
+  playerStore.deletePlayerSong(index)
+}
 </script>
 
 <style lang="less" scoped>
@@ -66,7 +66,7 @@ const handlePlayListItemClick = (index: number) => {
   .position(0, 0, 62px, auto);
   z-index: @z-index-player-list;
   background: var(--layout-content-bgcolor);
-  width: 450px;
+  width: 550px;
   height: calc(100vh - 62px);
   box-shadow: 5px 5px 5px 2px rgb(0, 0, 0, 0.2);
   padding: 15px 20px;
@@ -79,17 +79,15 @@ const handlePlayListItemClick = (index: number) => {
   }
   .header {
     .display-flex(space-between);
-    padding-bottom: 10px;
+    font-size: @font-size;
+    color: var(--font-color);
+    padding: 0 15px 15px;
     border-bottom: 1px solid var(--color-border);
-    .close {
-      cursor: pointer;
-    }
     .delete-all {
       .display-flex(@align-items: center);
-      font-size: @font-size;
       cursor: pointer;
       span {
-        padding-left: 3px;
+        padding-left: 5px;
         letter-spacing: 2px;
       }
     }
@@ -114,7 +112,7 @@ const handlePlayListItemClick = (index: number) => {
         }
         .order,
         .time {
-          width: 40px;
+          width: 60px;
         }
         .artists,
         .name {
